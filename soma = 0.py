8166fdb803143582615bbbd10dae2108039668f9
devdouglasbraga7 <devdouglasbraga7@gmail.com>
2026-04-17 23:19:08 -0300

    Transformei o código em clean code, utilizando laço de repetição para deixar escalável

--- soma = 0.py
+++ soma = 0.py
@@ -1,21 +1,14 @@
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite um número: "))
-n3 = int(input("Digite um número: "))
-n4 = int(input("Digite um número: "))
-n5 = int(input("Digite um número: "))
+contador_maior_10 = 0
+soma = 0
+media = 0
 
-soma = n1 + n2 + n3 + n4 + n5
-print(f"a soma entre os números é: {soma}")
-
-media = (n1 + n2 + n3 + n4 + n5)/5
-print(f"A média entre os números é: {media}")
-if n1 > 10:
-    print(f"O {n1} é maior que 10")
-if n2 > 10:
-    print(f"O {n2} é maior que 10")
-if n3 > 10:
-    print(f"O {n3} é maior que 10")
-if n4 > 10:
-    print(f"O {n4} é maior que 10")
-if n5 > 10:
-    print(f"O {n5} é maior que 10")
+for i in range(5):
+    numeros = int(input("digite um numero: "))
+    if numeros > 10:
+        contador_maior_10 += 1
+    soma += numeros
+    media = (soma)/5
+    
+print(f"A soma dos números é: {soma}")
+print(f"A média dos números é: {media}")
+print(f"A quantidade de números que são maiores que 10 é: {contador_maior_10}")
