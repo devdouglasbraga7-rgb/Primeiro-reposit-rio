db7337f8145c0f72cb2ec92a368d8e33d9dfd7c2
devdouglasbraga7 <devdouglasbraga7@gmail.com>
2026-05-16 15:40:44 -0300

    Adicionando Comentários para mostrar clareza no arquivo

--- listas/main.py/mini_crud_lista.py
+++ listas/main.py/mini_crud_lista.py
@@ -13,7 +13,7 @@ while loop == 0:
     print("="* 40)
 #Mostra as opções que o usuário pode fazer na lista
 
-    escolha = int(input("Escolha uma das opções digitando o número correspondente: "))
+    escolha = int(input("Escolha uma das opções digitando o número correspondente: ")) #Pede pro usuário escolher a opção desejada no menu
 
     if escolha == 1:
         clientes = input("Digite o nome do cliente: ")
@@ -39,6 +39,9 @@ while loop == 0:
         
         cadastro.remove(opcao)
         for nome in cadastro:
-            print("-", nome)       
-    loop = int(input("Digite 0 para continuar e 1 para sair"))
+            print("-", nome)
+
+ #Percorre a escolha do usuário           
+
+    loop = int(input("Digite 0 para continuar e 1 para sair: "))
 print("Obrigado por utilizar este programa!")
